599ce87ef09d586c3598e91176060c81a492d4da
Esther <esther.youri.shin@gmail.com>
2018-09-08 14:35:04 -0700

    added bracket that was missing which caused issues

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -15,27 +15,7 @@ export class CurrentWeatherComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.current = {
-      city: '',
-      country: '',
-      date: undefined,
-      image: '',
-      temperature: 0,
-      description: '',
-      id: 0
-    };
-
-    this.weatherService.getCurrentWeather('Seattle', 'us').subscribe((data) => {
-      this.current = {
-        city: data.name,
-        country: data.sys.country,
-        date: new Date(data.dt * 1000),
-        image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-        temperature: (data.main.temp * 9 / 5) - 459.67,
-        description: data.weather[0].description,
-        id: data.weather[0].id
-      }
-    })
+    this.weatherService.getCurrentWeather('Seattle', 'us').subscribe((data) => this.current = data)
   }
 
 }
